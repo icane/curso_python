ee808f279255a17a86e65c4574363899c1feceaa
JM Fernández <fernandez.cuesta@gmail.com>
2018-10-26 09:50:57 +0200

    PDF output command for build.sh

--- build.sh
+++ build.sh
@@ -2,3 +2,6 @@
 cat source/$1/*.md | \
 pandoc -t revealjs -f markdown -s - --css ../css/estilo.css  \
  -o slides/$1.html
+
+cat source/$1/*.md | grep -v theme | sed 's/incremental/.theme/' | \
+pandoc -t beamer -f markdown -s - -o slides/$1.pdf
